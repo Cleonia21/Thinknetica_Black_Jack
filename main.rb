b122d5a4dee8c1a6934266ad6df6612c482a3b3b
Cleonia <ChelseyLeonia@Cleonias-MacBook-Pro.local>
2021-12-23 18:20:15 +0300

    1

--- main.rb
+++ main.rb
@@ -2,4 +2,37 @@ require_relative 'interface'
 require_relative 'logics'
 require_relative 'player'
 
+class Card
+  def initialize(value, suit)
+    @value = value
+    @suit = suit
+  end
+end
+
+class Hand
+
+  attr_reader
+
+  def initialize(cards)
+    @cards = []
+    @cards << cards
+    @points = count_points
+  end
+
+  def count_points
+    @points = 0
+    @cards.each do |card|
+      value = card.value % 13
+      @points += card + 2 if card < 9
+      @points += 10 if [9, 10, 11].include?(card)
+      if @points + 11 <= 21 && card == 12
+        @points += 11
+      elsif card == 12
+        @points += 1
+      end
+    end
+    @points
+  end
+end
+
 Logics.new.process
--- player.rb
+++ player.rb
@@ -23,18 +23,5 @@ class Player
     @dollars += num
   end
 
-  def count_points
-    @points = 0
-    @cards.each do |card|
-      card %= 13
-      @points += card + 2 if card < 9
-      @points += 10 if [9, 10, 11].include?(card)
-      if @points + 11 <= 21 && card == 12
-        @points += 11
-      elsif card == 12
-        @points += 1
-      end
-    end
-    @points
-  end
+
 end
